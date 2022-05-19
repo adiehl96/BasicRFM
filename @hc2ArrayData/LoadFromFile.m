fabb3b6606d82cbe073ac75b22ffce7c4b4ddb6b
Arne Diehl <a.diehl@student.ru.nl>
2022-05-19 17:03:41 +0200

    Update LoadFromFile.m

--- @hc2ArrayData/LoadFromFile.m
+++ @hc2ArrayData/LoadFromFile.m
@@ -1,4 +1,4 @@
-function LoadFromFile( obj, filename, square, undirected, load_as_vectors )
+function LoadFromFile( obj, filename, square, undirected )
 %LOADFROMFILE Loads data
 % Detailed explanation goes here
 %
@@ -6,29 +6,8 @@ function LoadFromFile( obj, filename, square, undirected, load_as_vectors )
 
   % Save filename for future use
   obj.file_name = filename;  
-
-  if nargin < 5
-    load_as_vectors = false;
-  end
-  
-  % Check to see if filename exists
-  if isempty(filename) || (~exist(filename, 'file'))
-    obj.D_X = 0;
-    obj.M = cell(0);
-    obj.N = cell(0);
-    obj.train_X_v = cell(0);
-    obj.train_X_i = cell(0);
-    obj.train_X_j = cell(0);
-    obj.test_X_v = cell(0);
-    obj.test_X_i = cell(0);
-    obj.test_X_j = cell(0);
-    return;
-  end
-    
-  % Extract extension
-  [~, Ext] = strtok(filename, '.');
+     
   % Open file
-  if strcmp (Ext, '.csv')
     obj.D_X = 1;
     R = load(filename);
     R = MakeCell(R);
@@ -41,60 +20,30 @@ function LoadFromFile( obj, filename, square, undirected, load_as_vectors )
     obj.test_X_v  = cell(obj.D_X, 1);
     obj.test_X_i  = cell(obj.D_X, 1);
     obj.test_X_j  = cell(obj.D_X, 1);
+    %disp(obj.train_X_v)
     % Populate everything as training data
-    j = 0; % This counts the number of cells, if not equal to i
-    for i = 1:obj.D_X
-      if ~load_as_vectors
-        % Dimensions
-        [obj.M{i} obj.N{i}] = size(R{i});
-        % Training data
-        O = ones(size(R{i}));
-        if undirected && (size(R{i}, 1) == size(R{i}, 2)) && (all(all(R{i} == R{i}')))
-          % Undirected
-          O = triu(O);
-        end
-        if square && (size(R{i}, 1) == size(R{i}, 2))
-          O = O - diag(diag(O));
-        end
-        [obj.train_X_i{i} obj.train_X_j{i}] = find(O);
-        obj.train_X_v{i} = R{i}(O>0);
-        % Setup dummy test data
-        obj.test_X_i{i} = [];
-        obj.test_X_j{i} = [];
-        obj.test_X_v{i} = [];
-      else
-        % Split any matrices into vectors
-        % Dimensions
-        for k = 1:size(R{i}, 2)
-          j = j + 1; % Count which cell we are on. j > i when data presented as matrix
-          obj.M{j} = size(R{i}, 1);
-          obj.N{j} = 1;
-          % Training data
-          obj.train_X_i{j} = 1:size(R{i}, 1);
-          obj.train_X_j{j} = ones(size(R{i}, 1), 1);
-          obj.train_X_v{j} = R{i}(:, k);
-          % Setup dummy test data
-          obj.test_X_i{j} = [];
-          obj.test_X_j{j} = [];
-          obj.test_X_v{j} = [];
-        end
-      end
+    i = 1;
+    disp(i)
+    % Dimensions
+    [obj.M{i}, obj.N{i}] = size(R{i});
+    % Training data
+    O = ones(size(R{i}));
+    disp(size(O))
+    if undirected && (size(R{i}, 1) == size(R{i}, 2)) && (all(all(R{i} == R{i}')))
+      disp("condition held")
+      % Undirected
+      O = triu(O);
     end
-    if load_as_vectors
-      % Were there any hiding dimensions?
-      obj.D_X = j; % What count did we get to?
+    disp(sum(sum(O)))
+    if square && (size(R{i}, 1) == size(R{i}, 2))
+      O = O - diag(diag(O));
     end
-  elseif strcmp (Ext, '.mat')
-    temp = load(filename, 'data');
-    obj.M         = temp.data.M;
-    obj.N         = temp.data.N;
-    obj.train_X_v = temp.data.train_X_v;
-    obj.train_X_i = temp.data.train_X_i;
-    obj.train_X_j = temp.data.train_X_j;
-    obj.test_X_v  = temp.data.test_X_v;
-    obj.test_X_i  = temp.data.test_X_i;
-    obj.test_X_j  = temp.data.test_X_j;
-    clear temp;
-  end 
+    disp(sum(sum(O)))
+    [obj.train_X_i{i}, obj.train_X_j{i}] = find(O);
+    
+    obj.train_X_v{i} = R{i}(O>0);
+    % Setup dummy test data
+    obj.test_X_i{i} = [];
+    obj.test_X_j{i} = [];
+    obj.test_X_v{i} = [];
 end
-
